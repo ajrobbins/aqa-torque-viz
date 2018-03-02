31a91fbb65794d5587eff670b416fe9c1d38f479
arobbi7 <arobbins@azavea.com>
2018-03-02 10:15:03 -0500

    Include comments

--- js/main.js
+++ js/main.js
@@ -31,28 +31,41 @@ var CARTOCSS = [
         attribution: 'CartoDB'
       }).addTo(map);
 
-      // var layerSource = {
-      //     type: 'torque',
-      //     options: {
-      //         query: "SELECT * FROM " + "data_w_geom",
-      //         user_name: "ariannarobbins",
-      //         cartocss: CARTOCSS
-      //     }
-      // };
-      var layerSource = {
-        user_name: 'ariannarobbins',
-        type: 'cartodb',
-        sublayers: [
-   {
-     sql: 'select * from all_day',
-     cartocss: '#layer { marker-fill:#4682B4; }'
-   },
-   {
-    sql: 'select * from data_w_geom',
-    cartocss: '#layer { marker-fill: #FFB927; }'
-   },
- ],
+      var torqueLayerSource = {
+          type: 'torque',
+          options: {
+              query: "SELECT * FROM " + "data_w_geom",
+              user_name: "ariannarobbins",
+              cartocss: CARTOCSS
+          }
       };
+
+      var otherLayerSource = {
+          user_name: "ariannarobbins",
+          type: "cartodb",
+          options: {
+            sql: "SELECT * FROM all_day",
+            cartocss: '#layer { marker-fill: #FFB927; }'
+          }
+      };
+//       var layerSource = {
+//         user_name: 'ariannarobbins',
+//         type: 'cartodb',
+//         sublayers: [
+//    {
+//     sql: 'select * from all_day',
+//     cartocss: '#layer { marker-fill: #FFB927; }'
+//    },
+//    {
+//   // type: 'torque', // Required
+//   options: {
+//     query: "SELECT * FROM " + "data_w_geom",   // Required if table_name is not given
+//     user_name: "ariannarobbins", // Required
+//     cartocss: CARTOCSS // Required
+//   }
+// }
+//  ],
+//       };
 
       var selectedLayer;
             // create layer selector
@@ -69,59 +82,120 @@ var CARTOCSS = [
                   // ease of understanding
                   if (layer == 'abc'){
                     layers.getSubLayer(0).show(); // countries
-                    layers.getSubLayer(1).show(); // cables
-                    layers.getSubLayer(2).show(); // populated places
+                    layers.getSubLayer(1).hide(); // cables
+                    // layers.getSubLayer(2).show(); // populated places
                   }
                   else if (layer == 'efg') {
-                    layers.getSubLayer(0).show();
-                    layers.getSubLayer(1).hide();
-                    layers.getSubLayer(2).show();
+                    layers.getSubLayer(0).hide();
+                    layers.getSubLayer(1).show();
+                    // layers.getSubLayer(2).show();
+                    // var torqueLayer = layer;
+                    // torqueLayer.pause();
+                    //
+                    // torqueLayer.on('load', function() {
+                    //     torqueLayer.play();
+                    // });
+                    //
+                    // // pause animation at last frame
+                    // torqueLayer.on('change:time', function(changes) {
+                    //     if (changes.step === torqueLayer.provider.getSteps() - 1) {
+                    //         torqueLayer.pause();
+                    //     }
+                    // });
                   }
                   else {
                     layers.getSubLayer(0).show();
                     layers.getSubLayer(1).show();
-                    layers.getSubLayer(2).hide();
+                    // layers.getSubLayer(2).hide();
                   }
                 }
               });
             }
             selectedStyle = $('li.selected').attr('id');
 
-      cartodb.createLayer(map, layerSource, { filter: ['http', 'mapnik'] })
-        .addTo(map)
-        .done(function(layers) {
-          createSelector(layers);
+            function torqueLayerCallBack(torqueLayer) {
+              torqueLayer.pause();
 
-            // var torqueLayer = layer;
-            // torqueLayer.pause();
-            //
-            // torqueLayer.on('load', function() {
-            //     torqueLayer.play();
-            // });
-            //
-            // // pause animation at last frame
-            // torqueLayer.on('change:time', function(changes) {
-            //     if (changes.step === torqueLayer.provider.getSteps() - 1) {
-            //         torqueLayer.pause();
-            //     }
-            // });
-            $('#target-button').click(function() {
-              console.log("targeting on");
-              torqueLayer.hide();
-              torqueLayer.stop();
-              $('.cartodb-timeslider').hide();
-              // layer.getSubLayer(0).setSQL('SELECT * FROM geom_data_js_v2 WHERE wtarg = 1');
+              torqueLayer.on('load', function() {
+                  torqueLayer.play();
+              });
+
+              // pause animation at last frame
+              torqueLayer.on('change:time', function(changes) {
+                  if (changes.step === torqueLayer.provider.getSteps() - 1) {
+                      torqueLayer.pause();
+                  }
+              });
+
+                $('#target-button').click(function() {
+                  console.log("targeting on");
+                  torqueLayer.hide();
+                  torqueLayer.stop();
+                  $('.cartodb-timeslider').hide();
+                  // layer.getSubLayer(0).setSQL('SELECT * FROM geom_data_js_v2 WHERE wtarg = 1');
+                });
+
+
+                $('#reset-button').click(function() {
+                  console.log("reset!");
+                  torqueLayer.show();
+                  torqueLayer.play();
+                  $('.cartodb-timeslider').show();
+                  // layer.getSubLayer(0).setSQL('SELECT * FROM geom_data_js_v2 WHERE wtarg = 1');
+                });
+            }
+
+            var q = queue(3);
+            q.defer(function(torqueLayerSource, torqueLayerCallBack) {
+                cartodb.createLayer(map, torqueLayerSource, function(layer) { torqueLayerCallBack( layer); });
+            });
+            q.defer(function(otherLayerSource) {
+            cartodb.createLayer(map, otherLayerSource);
             });
 
-
-            $('#reset-button').click(function() {
-              console.log("reset!");
-              torqueLayer.show();
-              torqueLayer.play();
-              $('.cartodb-timeslider').show();
-              // layer.getSubLayer(0).setSQL('SELECT * FROM geom_data_js_v2 WHERE wtarg = 1');
-            });
-        })
-        .error(function(err) {
-            console.log("Error: " + err);
-        });
+            q.await(function() {
+                      var layers = Array.prototype.slice.call(arguments, 1);
+                      createSelector(layers);
+                      layers.forEach(function(layer) {
+                        layer.addTo(map);
+                      });
+                    });
+      // cartodb.createLayer(map, layerSource)
+      //   .addTo(map)
+      //   .done(function(layers) {
+      //     createSelector(layers);
+      //
+      //     var torqueLayer = layers[1];
+      //     torqueLayer.pause();
+      //
+      //     torqueLayer.on('load', function() {
+      //         torqueLayer.play();
+      //     });
+      //
+      //     // pause animation at last frame
+      //     torqueLayer.on('change:time', function(changes) {
+      //         if (changes.step === torqueLayer.provider.getSteps() - 1) {
+      //             torqueLayer.pause();
+      //         }
+      //     });
+      //
+      //       $('#target-button').click(function() {
+      //         console.log("targeting on");
+      //         torqueLayer.hide();
+      //         torqueLayer.stop();
+      //         $('.cartodb-timeslider').hide();
+      //         // layer.getSubLayer(0).setSQL('SELECT * FROM geom_data_js_v2 WHERE wtarg = 1');
+      //       });
+      //
+      //
+      //       $('#reset-button').click(function() {
+      //         console.log("reset!");
+      //         torqueLayer.show();
+      //         torqueLayer.play();
+      //         $('.cartodb-timeslider').show();
+      //         // layer.getSubLayer(0).setSQL('SELECT * FROM geom_data_js_v2 WHERE wtarg = 1');
+      //       });
+      //   })
+      //   .error(function(err) {
+      //       console.log("Error: " + err);
+      //   });
