f7634d63b50d568eb7bc63defdf0c650adc3b95d
arobbi7 <arobbins@azavea.com>
2018-09-23 21:11:45 -0400

    Minor styling changes

--- js/main.js
+++ js/main.js
@@ -90,6 +90,7 @@ window.onload = function() {
     .done(function(torqueLayer) {
       torqueLayer.pause();
       torqueLayer.on("load", function() {
+        torqueLayer.play();
       });
 
       //pause animation at last frame
@@ -98,6 +99,21 @@ window.onload = function() {
           torqueLayer.pause();
         }
       });
+
+      $("#target-button").click(function() {
+        console.log("targeting on");
+        torqueLayer.hide();
+        torqueLayer.stop();
+        $(".cartodb-timeslider").hide();
+      });
+
+      $("#reset-button").click(function() {
+        console.log("reset!");
+        torqueLayer.show();
+        torqueLayer.play();
+        $(".cartodb-timeslider").show();
+      });
+    });
 
   function selectInitialSublayer(layer) {
     var isInitialViolent = $("ul")
@@ -122,6 +138,7 @@ window.onload = function() {
     if (violent) {
       $(densityLegendNon.render().el).hide();
       $(densityLegend.render().el).show();
+      console.log("violent");
     } else {
       $(densityLegend.render().el).hide();
       $(densityLegendNon.render().el).show();
